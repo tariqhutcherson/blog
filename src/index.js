4edfc657e1714ae52edfea096f6e5f9634b0bc74
Tariq Hutcherson <tariqhutcherson@gmail.com>
2025-02-09 11:57:41 -0500

    New

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ScrollToTop from "./ScrollToTop";
@@ -9,10 +9,10 @@ import ScrollToTop from "./ScrollToTop";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
